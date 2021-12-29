caa3de86da97bb105052c5c4c4006b8014374ab7
Roman Hyrych <88936471+romanlancer@users.noreply.github.com>
2021-12-29 03:52:58 +0200

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from './gallery-items.js'
 // Change code below this line
+// console.log(galleryItems)
+const galleryEl = document.querySelector('.gallery')
 
-console.log(galleryItems);
+const galleryCardsMarkup = createGalleryCardsMarkup(galleryItems)
+galleryEl.insertAdjacentHTML('beforeend', galleryCardsMarkup)
+
+// console.log(createGalleryCardsMarkup(galleryItems))
+
+function createGalleryCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+         <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+         </a>
+        </div >
+        `
+    })
+    .join('')
+}
+
+galleryEl.addEventListener('click', onPictureClick)
+
+function onPictureClick(e) {
+  e.preventDefault()
+  if (!e.target.classList.contains('gallery__image')) {
+    return
+  }
+  // console.log(event.target);
+  // window.addEventListener('keydown', onEscBtnPress)
+  // modalRef.classList.add('is-open')
+  imageRef.src = e.target.dataset.source
+  const instance = basicLightbox.create(
+    document.querySelector('.gallery > gallery__image')
+  )
+
+  instance.show()
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,11 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from './gallery-items.js'
 // Change code below this line
 
-console.log(galleryItems);
+console.log(galleryItems)
+const instance = basicLightbox.create(`
+    <video controls>
+        <source src="assets/videos/video.mp4" type="video/mp4">
+    </video>
+`)
+
+instance.show()
